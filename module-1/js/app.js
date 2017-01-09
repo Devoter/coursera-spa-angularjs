2faa788eb4410ae5e249097c0abe959016254fd7
Alexey Nosov <nayretech@gmail.com>
2017-01-09 10:19:30 +0700

    module-1 Complete assignment

--- module-1/js/app.js
+++ module-1/js/app.js
@@ -1,10 +1,36 @@
+/* jshint esversion: 6 */
+
 (function() {
-angular.module('LunchChecker', [])
+'use strict';
 
-.controller('CheckerController', CheckerController);
+angular.module('LunchCheck', [])
 
-CheckerController.$inject = ['$scope'];
-function CheckerController($scope) {
-    
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+    $scope.menu = '';
+    $scope.message = '';
+    $scope.state = '';
+
+    $scope.checkIfTooMuch = function() {
+        let menu = $scope.menu.split(',');
+        let itemsCount = 0;
+
+        menu.forEach((item) => {
+            if (item.trim() !== '') {
+                ++itemsCount;
+            }
+        });
+
+        if (itemsCount) {
+            $scope.message = itemsCount > 3 ? 'Too much!' : 'Enjoy!';
+            $scope.state = 'alert-success';
+        }
+        else {
+            $scope.message = 'Please enter data first';
+            $scope.state = 'alert-danger';
+        }
+    };
 }
 })();
