1b4d06ee1de02df2c321371dccf826416ad54206
Alexey Nosov <nayretech@gmail.com>
2017-01-09 09:49:03 +0700

    module-1 Add application skeleton script

---
+++ module-1/js/app.js
@@ -0,0 +1,10 @@
+(function() {
+angular.module('LunchChecker', [])
+
+.controller('CheckerController', CheckerController);
+
+CheckerController.$inject = ['$scope'];
+function CheckerController($scope) {
+    
+}
+})();
